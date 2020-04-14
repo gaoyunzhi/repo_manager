189a6a22af8de36453a8083c546da949c5ebb23e
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-14 14:04:07 -0700

    commit

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
 import os
-import sys
+import threading
+
+def readFile(fn):
+	result = {}
+	with open(fn + '.txt') as f:
+		for x in f.readlines():
+			x = x.strip()
+			if not x:
+				continue
+			x = x.split()
+			result[x[0]] = ' '.join(x[1:])
+	return result
 
 def kill():
-	os.system("ps aux | grep ython | grep weibo_aggregator | awk '{print $2}' | xargs kill -9")
+	os.system("ps aux | grep ython | grep setup | awk '{print $2}' | xargs kill -9")
 
-def setup():
-	kill()
-	if 'kill' in str(sys.argv):
-		return 
-	addtional_arg = ' '.join(sys.argv[1:])
-	command = 'python3 -u weibo_aggregator.py %s' % addtional_arg
-	if 'debug' in addtional_arg or 'skip' in addtional_arg or 'once' in addtional_arg:
-		os.system(command + ' test')
-	else:
-		os.system('nohup %s &' % command)
-		if 'notail' not in addtional_arg:
-			os.system('touch nohup.out && tail -F nohup.out')
+def loopImp():
+	r = os.system('pip3 install --user all_dependencies.txt --upgrade')
+	print(r)
+	config = readFile('config')
+	repo_names = readFile('repo_names')
+
+def loop():
+	loopImp()
+	threading.Timer(60 * 10, loop).start() 
 
 if __name__ == '__main__':
-	setup()
+	kill()
+	loop()
