85c844d393b3923616ab90342cba4aecf90fad93
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-14 14:54:19 -0700

    commit

--- repo_manager.py
+++ repo_manager.py
@@ -2,6 +2,9 @@ import os
 import threading
 import yaml
 
+def kill(name):
+	os.system("ps aux | grep ython | grep %s | awk '{print $2}' | xargs kill -9" % name)
+
 def readFile(fn):
 	result = {}
 	with open(fn + '.txt') as f:
@@ -13,23 +16,31 @@ def readFile(fn):
 			result[x[0]] = ' '.join(x[1:])
 	return result
 
+def process(dirname, runner_name, config):
+	if not config.get('no_auto_commit'):
+		os.popen('cd ../%s && git add . && git commit -m commit && git push -u -f' % dirname)
+	r = os.popen('cd ../%s && git fetch origin && git rebase origin/master && git push -u -f' % dirname).read()
+	if ('up to date' not in r or dep_installed) and not config.get('no_restart'):
+		kill(runner_name)
+
 def loopImp():
 	r = os.popen('pip3 install --user -r all_dependencies.txt --upgrade').read()
-	if 'Successfully installed' in r:
-		print('~~~Successfully installed~~')
-	else:
-		print('no new')
+	dep_installed = 'Successfully installed' in r
+
 	with open('config.yaml') as f:
 		config = yaml.load(f, Loader=yaml.FullLoader)
+
 	repo_names = readFile('repo_names')
+
 	for dirname in repo_names:
 		runner_name = repo_names[dirname]
 		config = config.get(runner_name, {})
-		print(config.get('no_auto_commit'))
-		if not config.get('no_auto_commit'):
-			os.popen('cd ../%s && git add . && git commit -m commit && git push -u -f' % dirname)
-		r = os.popen('cd ../%s && git fetch origin && git rebase origin/master && git push -u -f' % dirname)
-		print('pull result', r)
+		process(dirname, runner_name, config)
+			
+
+
+
+
 
 
 
