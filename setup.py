b00e87eb9bcb2b33d3604e4f6d667beecce5cf8b
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-09 05:53:29 -0700

    commit

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import os
+import sys
 
 def kill():
 	os.system("ps aux | grep ython | grep repo_manager | awk '{print $2}' | xargs kill -9")
 
 if __name__ == '__main__':
 	kill()
+	if 'kill' in sys.argv:
+		return
 	os.system('nohup python3 -u repo_manager.py &')
 	os.system('touch nohup.out')
 	os.system('tail -F nohup.out')
