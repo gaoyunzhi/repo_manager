f1d05944c6fe0bffc52f406308f0f58f3243f237
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-02 08:24:56 -0700

    commit

--- repo_manager.py
+++ repo_manager.py
@@ -69,6 +69,7 @@ def process(dirname, runner_name, config, dep_installed):
 		os.popen('cd ../%s && git add . && git commit -m commit && git push -u -f' % dirname).read()
 
 	r = os.popen('cd ../%s && git fetch origin && git rebase origin/master && git push -u -f' % dirname).read()
+	print('process1', r)
 	if (('up to date' not in r and 'commit or stash them' not in r) or \
 		dep_installed) and okToRestart(config):
 		kill(runner_name)
